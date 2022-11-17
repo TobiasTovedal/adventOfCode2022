07ac896955a8e94a6acee6c5cf628b3e70187d6c
Tobias Tovedal <tobias.tovedal@gmail.com>
2022-11-17 14:24:17 +0100

    2nd

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import numpy as np
 
-teeest
+f = open("input.txt", "r")
+values = np.array([])
+
+for line in f:
+    values = np.append(values, int(line))
+
+for numOne in values:
+    for numTwo in values:
+        for numThree in values:
+            if (numOne + numTwo + numThree == 2020):
+                print(numOne * numTwo * numThree)
+
