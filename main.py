024363c750f2d51591f282c4b0a6144e5b287afc
Tobias Tovedal <tobias.tovedal@gmail.com>
2022-12-01 13:01:14 +0100

    Day 1

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import numpy as np
+import re
 
 f = open("input.txt", "r")
-values = np.array([])
+elfs = np.array([])
+elfIndex = 0
+elfCalSum = 0
 
 for line in f:
-    values = np.append(values, int(line))
+    if line != "\n":
+        elfCalSum = elfCalSum + int(line)
+    else:
+        elfs = np.append(elfs, elfCalSum)
+        elfCalSum = 0
+        elfIndex = elfIndex + 1
 
-for numOne in values:
-    for numTwo in values:
-        for numThree in values:
-            if (numOne + numTwo + numThree == 2020):
-                print(numOne * numTwo * numThree)
-
+print(np.sort(elfs))
+print(71780 + 71481 + 69228)
