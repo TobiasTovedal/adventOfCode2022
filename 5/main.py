b5939200260b39c57de058cbfd9c4adb59fb31b7
tobias <tobias.tovedal@gmail.com>
2022-12-06 19:59:27 +0100

    Day 5

---
+++ 5/main.py
@@ -0,0 +1,21 @@
+file = open("5/input.txt", "r")
+
+def finder(line):
+    packetLength = 14
+    i = 0
+    while i < len(line) - packetLength:
+        marker = line[i:i+packetLength] 
+        numberOfOccurances = 0
+
+        for character in marker:
+            numberOfOccurances = numberOfOccurances + marker.count(character)
+        
+        if numberOfOccurances == packetLength: 
+            print(marker, i + packetLength) 
+            break
+
+        i = i + 1
+
+for line in file:
+    finder(line)
+    
